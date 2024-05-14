71af2f7d749e585c7bc2fefbee46ce8ec5da43d3
adewale-codes <adewaleabiola78@gmail.com>
2024-05-14 21:05:24 +0100

    changed navbar to be responsive

--- src/Conponent/Nav/Navbar.jsx
+++ src/Conponent/Nav/Navbar.jsx
@@ -1,61 +1,48 @@
-import React, { useEffect, useState } from "react";
-import "./Navbar.css";
-import { CgMenuGridO } from "react-icons/cg";
-import { FaRegTimesCircle } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState } from "react"; 
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"; 
 
 const Navbar = () => {
+  const location = useLocation();
+  const pathName = location.pathname;
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="header">
-      <div className="logoSpace">
-        <img
-          src="https://s3-alpha-sig.figma.com/img/f73e/c525/34ddeb66be0b89dcad920fba0d323c96?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pMYWtZk5a6NfxqW6OWjAnz50w-C6yL2T3XdWZhoRv8OrTJadXsAaPs7MyzWao1fKWKw~-r-V1krTZx8QmHe7KAFm8TIYKeEWYODLOZMT0iJRZRjsC~WKNJ1VRKCVuUb0adTJuFhy-MbTccYK5awxwjdhTFKK0q8SKoKfJE~NPI8PMqKI7SdiAF-v15TfWkYwlEvlV-QuzlXn9765MEar6CDvfOez4F9lqRuc0mANYGa3Ti157Wqrcpnxjp1Nwh2NQYLtIRE9VGB-RExzURbwnF~JTzXEsGqGJmbHpycg1Tqd2pcfkM1814xgV5-4HImZ7DpekYrMA8y4Um9yZxJi6g__"
-          alt=""
-        />
-        <div className="logoName">
-          <div>
-            <span className="short">b</span>
-            <span className="long">better</span>
-          </div>
-          <div>
-            <span className="short">d</span>
-            <span className="long">days</span>
-          </div>
-          <div>
-            <span className="short">t</span>
-            <span className="long">through</span>
-          </div>
-          <div>
-            <span className="short">c</span>
-            <span className="long">crypto</span>
-          </div>
+    <nav>
+      <div className="bg-black grid lg:grid-cols-[200px_minmax(400px,_1fr)_200px] grid-cols-[1fr_50px] items-center lg:px-28 px-4 py-5 h-24">
+        <Link to="/">
+          <div>LOGO</div>
+        </Link>
+        <div className="justify-self-start lg:pl-16 hidden lg:flex items-center justify-center gap-2 md:gap-8">
+          <Link className={pathName === "/schools" ? "" : "text-secondary-400"} to='/schools'>Schools</Link>
+          <Link className={pathName === "/tutorials" ? "" : "text-secondary-400"} to='/tutorials'>Tutorials</Link>
+          <Link className={pathName === "/how-it-works" ? "" : "text-secondary-400"} to='/how-it-works'>How it works</Link>
+          <Link className={pathName === "/about" ? "" : "text-secondary-400"} to='/about'>About</Link>
+        </div>
+        <Link to="/join" className="hidden lg:flex">
+          <div></div>
+        </Link>
+        <div className='lg:hidden flex justify-self-end cursor-pointer'>
+          {isOpen ? (
+            <XMarkIcon className='h-6 w-6' onClick={() => setIsOpen(false)} />
+          ) : (
+            <Bars3Icon className='h-6 w-6' onClick={() => setIsOpen(true)} />
+          )}
         </div>
       </div>
-      <CgMenuGridO className="menu" />
-      <div className="navBars">
-        <FaRegTimesCircle className="times" />
-        <div className="navItems">
-          <ul>
-            <Link to={"/"} className="li">
-              Home
-            </Link>
-            <Link className="li" to={"/service"}>
-              Project
-            </Link>
-            <Link to={"/project"} className="li">
-              Service
-            </Link>
-            <Link to={"/team"} className="li">
-              Contact
-            </Link>
-          </ul>
+      <div className={`block lg:hidden fixed h-64 -mt-4 transform left-0 w-full bg-white transition-transform duration-300 ease-in-out overflow-auto ${isOpen ? 'translate-x-0' : '-translate-x-full'} lg:relative lg:translate-x-0 lg:justify-self-start lg:pl-32 lg:flex lg:items-center lg:justify-center lg:gap-2 lg:md:gap-8 lg:bg-transparent`}>
+        <div className="flex flex-col space-y-5 ml-5">
+          <Link to='/schools'>Schools</Link>
+          <Link to='/tutorials'>Tutorials</Link>
+          <Link to='/how-it-works'>How it works</Link>
+          <Link to='/about'>About</Link>
+          <Link to="/join">
+            <div></div>
+          </Link>
         </div>
-        <a href="#" className="book">
-          Book Now
-        </a>
       </div>
-    </div>
+    </nav>
   );
-};
+}
 
 export default Navbar;
